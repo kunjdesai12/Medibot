7165934bc80c84e18ded4108faff3649ed40e010
Kunjcoder <desaikunj125@gmail.com>
2025-10-14 12:14:07 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author= "Kunj Desai",
+    author_email="kunjd7423@gmail.com ",
+    packages=find_packages(),
+    install_requires=[]
+    )
